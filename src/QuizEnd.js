cf4e7e7e463cd68fe45d340c40f4554de1d56fc8
NV <nikola.vrbinarova@accenture.com>
2021-09-10 08:45:25 +0200

    4 commit update

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class QuizEnd extends Component {
     render() {
@@ -10,4 +10,4 @@ class QuizEnd extends Component {
         )
     }
 }
-export default  QuizEnd
+export default QuizEnd;
